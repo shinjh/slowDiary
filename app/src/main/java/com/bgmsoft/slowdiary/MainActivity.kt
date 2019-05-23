64c9358e8dae6dfb50013058ea525f3dd1dbbd56
Jaeho shin <sjh1253@hotmail.com>
2019-05-23 10:01:18 +0900

    Firebase Analytics 추가

--- app/src/main/java/com/bgmsoft/slowdiary/MainActivity.kt
+++ app/src/main/java/com/bgmsoft/slowdiary/MainActivity.kt
@@ -2,11 +2,25 @@ package com.bgmsoft.slowdiary
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
+
+    fun setLogEvent(id: String, name: String) {
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id)
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        mFirebaseAnalytics?.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
